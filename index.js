261e3a3f77861bafa87db1503f985264c8f16a93
Un Known <jasonqan2004@gmail.com>
2016-11-01 06:25:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var http = require("http");
-var server = http.createServer((req, res) => { res.end("lol nothing here what are you looking at"); });
-server.listen(8080, function(){});
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('index');
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
